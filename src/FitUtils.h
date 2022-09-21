45ff54a65b06a2407cad215bb19c827dbfb6c63a
Petr Stepanov <stepanovps@gmail.com>
2022-09-21 09:36:15 -0400

    API readme update

--- src/FitUtils.h
+++ src/FitUtils.h
@@ -11,16 +11,16 @@
 #include <RtypesCore.h>
 #include <TF1.h>
 
-class CrystalBallFunctionObject {
-  public:
-    CrystalBallFunctionObject(Bool_t _isInverted = kFALSE);
-    Double_t operator()(double *_x, double *par);
-
-  private:
-    Bool_t isReversed;
-};
-
 namespace FitUtils {
+
+  class CrystalBallFunctionObject {
+    public:
+      CrystalBallFunctionObject(Bool_t _isInverted = kFALSE);
+      Double_t operator()(double *_x, double *par);
+
+    private:
+      Bool_t isReversed;
+  };
 
   TF1* fitHistWithROOTCrystalBall(TH1 *hist, Bool_t inverted);
   TF1* getCrystalBallFunction(TH1 *hist, Bool_t reversed = kFALSE);
